aebf2d051488fcdfc6de857dd63baab8eb069ce8
mmhennessy <mhennessy@mcad.edu>
2015-10-08 20:32:26 -0500

    Initial Commit
    Files for Exercise 7.1

---
+++ script.js
@@ -0,0 +1,9 @@
+var main = document.getElementByID("text");
+
+console.log("Hail Javascript!");
+
+var list = document.getElementsByTagName("Li");
+
+console.log("My list of sports just for practice");
+
+myElement.setAttribute("align", "center");
